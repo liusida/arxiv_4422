762a83fe2dc1fee3606f28755433024702df7724
sida <sliu1@uvm.edu>
2021-05-02 23:12:31 -0400

    save

--- exp.sh
+++ exp.sh
@@ -2,14 +2,16 @@
 # This is for running experiments on the cloud
 set -x
 expName="CPUvsGPU"
-if [$1 -eq 'cpu'] then
+if [$1 -eq 'cpu']
+then
     for seed in {1..100}
     do
         sbatch -J $expName ~/bin/bluemoon.sh ds2 python 3.minLA_cpu.py --seed=$seed --tag=$1
     done
 fi
 
-if [$1 -eq 'gpu'] then
+if [$1 -eq 'gpu']
+then
     for seed in {1..100}
     do
         sbatch -J $expName ~/bin/deepgreen.sh ds2 python 4.minLA_gpu.py --seed=$seed --tag=$2
